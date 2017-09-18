0f056bcae011b9450afc3c4c15d22254d9c5a1e5
Ryan Shin <changyeolshin@gmail.com>
2017-09-18 10:49:46 -0700

    Refactored JS code for adding and clearing choices. #13

--- main.js
+++ main.js
@@ -1,11 +1,80 @@
 /* eslint-disable no-unused-vars */
-var Typed = require('typed.js')
+var choices = []
+var choiceId
+
+function choice($userChoiceInput, $userNameInput) {
+  var $choiceContainer = document.createElement('div')
+  var $choiceContent = document.createElement('li')
+  $choiceContainer.setAttribute('class', 'choice col s8 offset-s2')
+  $choiceContainer.setAttribute('data-id', choiceId)
+  $choiceContent.setAttribute('class', 'card-panel animated bounceInUp')
+  $choiceContent.textContent = $userChoiceInput.value
+  $choiceContainer.appendChild($choiceContent)
+
+  var choiceInfo = {
+    id: choiceId,
+    choice: $userChoiceInput.value,
+    author: $userNameInput.value,
+    htmlElement: $choiceContainer
+  }
+  choiceId++
+  return choiceInfo
+}
+
+function renderChoices(data) {
+  var $choices = []
+  for (var i = 0; i < data.length; i++) {
+    $choices.push(data[i].htmlElement)
+  }
+  return $choices
+}
+
+function addChoicesToList($choices) {
+  var $choicesList = document.querySelector('#choices-list')
+  for (var i = 0; i < $choices.length; i++) {
+    $choicesList.appendChild($choices[i])
+  }
+}
+
+function clearAllChoices() {
+  var $choicesList = document.querySelector('#choices-list')
+  while ($choicesList.firstChild) {
+    $choicesList.removeChild($choicesList.firstChild)
+  }
+}
+
+function clearUserInput($userChoiceInput, $userNameInput) {
+  $userChoiceInput.value = ''
+  $userNameInput.value = ''
+}
 
 var $submitButton = document.querySelector('#submit')
-var $clearButton = document.querySelector('#clear')
-var $randomizeButton = document.querySelector('#randomize')
+$submitButton.addEventListener('click', function (e) {
+  e.preventDefault()
+  var $userChoiceInput = document.querySelector('#user-choice')
+  var $userNameInput = document.querySelector('#user-name')
+  var newChoice = choice($userChoiceInput, $userNameInput)
+  choices.push(newChoice)
+  var $choices = renderChoices(choices)
+  addChoicesToList($choices)
+  // clearUserInput($userChoiceInput, $userNameInput)
+})
 
-var header = new Typed('.header', {
+var $clearButton = document.querySelector('#clear')
+$clearButton.addEventListener('click', function (e) {
+  e.preventDefault()
+  choices = []
+  clearAllChoices()
+})
+
+var $randomizeButton = document.querySelector('#randomize')
+$randomizeButton.addEventListener('click', function (e) {
+  e.preventDefault()
+})
+
+var Typed = require('typed.js')
+
+var header = new Typed('.app-header', {
   strings: ["Let's grab: McDonalds", "Let's grab: Burger King", "Let's grab: Pizza Hut", 'To Be Determined...'],
   typeSpeed: 50,
   backSpeed: 50,
@@ -13,70 +82,8 @@ var header = new Typed('.header', {
   showCursor: false
 })
 
-var slogan = new Typed('.slogan', {
+var slogan = new Typed('.app-slogan', {
   strings: ["^10000 Can't decide? Let us decide."],
   typeSpeed: 80,
   showCursor: false
-})
-
-function renderList($unorderedList, $list) {
-  var $userChoice = document.querySelector('#user-choice')
-  var $userName = document.querySelector('#user')
-  if ($userChoice.value !== '' && $userName.value !== '') {
-    var $removeButton = document.createElement('button')
-
-    $removeButton.setAttribute('class', 'waves-effect waves-circle waves-light btn-floating secondary-content')
-    $removeButton.setAttribute('id', 'remove')
-
-    $list.textContent = $userChoice.value
-    $removeButton.textContent = 'X'
-
-    $userChoice.value = ''
-    $userName.value = ''
-
-    $unorderedList.appendChild($list)
-    $list.appendChild($removeButton)
-
-    $removeButton.addEventListener('click', function (e) {
-      $unorderedList.removeChild($list)
-    })
-  }
-}
-
-function clearList($unorderedList, $lists) {
-  for (var i = 0; i < $lists.length; i++) {
-    $unorderedList.removeChild($lists[i])
-  }
-}
-
-function randomizeList($unorderedList, $lists) {
-  var index = Math.round(Math.random() * ($lists.length - 1))
-
-  for (var i = 0; i < $lists.length; i++) {
-    if (index !== i) {
-      $unorderedList.removeChild($lists[i])
-    }
-  }
-}
-
-$submitButton.addEventListener('click', function (e) {
-  var $choiceList = document.querySelector('.choice-list')
-  var $choice = document.createElement('li')
-  $choice.setAttribute('class', 'choice col s12 animated bounceInUp')
-  renderList($choiceList, $choice)
-})
-
-$clearButton.addEventListener('click', function (e) {
-  var $choiceList = document.querySelector('.choice-list')
-  var $choices = document.querySelectorAll('.choice')
-  clearList($choiceList, $choices)
-})
-
-$randomizeButton.addEventListener('click', function (e) {
-  var $choiceList = document.querySelector('.choice-list')
-  var $choices = document.querySelectorAll('.choice')
-
-  if ($choices.length > 1) {
-    randomizeList($choiceList, $choices)
-  }
 })
