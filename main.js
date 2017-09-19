572f623b9a7f4972c0cf8abd5a16b7721cba8814
Ryan Shin <changyeolshin@gmail.com>
2017-09-19 10:24:22 -0700

    Refactored JS code for removing a choice and choosing a random choice. #13

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 /* eslint-disable no-unused-vars */
 var choices = []
-var choiceId
+var choiceId = 0
 
-function choice($userChoiceInput, $userNameInput) {
-  var $choiceContainer = document.createElement('div')
-  var $choiceContent = document.createElement('li')
-  $choiceContainer.setAttribute('class', 'choice col s8 offset-s2')
-  $choiceContainer.setAttribute('data-id', choiceId)
-  $choiceContent.setAttribute('class', 'card-panel animated bounceInUp')
-  $choiceContent.textContent = $userChoiceInput.value
-  $choiceContainer.appendChild($choiceContent)
-
-  var choiceInfo = {
-    id: choiceId,
-    choice: $userChoiceInput.value,
-    author: $userNameInput.value,
-    htmlElement: $choiceContainer
+function choice(data) {
+  if (data) {
+    var $choice = document.createElement('li')
+    $choice.setAttribute('class', 'choice card-panel animated bounceInUp')
+    $choice.setAttribute('data-id', choiceId)
+    $choice.textContent = data.userChoice
+    return $choice
   }
-  choiceId++
-  return choiceInfo
+}
+
+function createChoiceInfo($userChoiceInput, $userNameInput) {
+  if ($userChoiceInput.value && $userNameInput.value) {
+    var choiceInfo = {
+      id: choiceId,
+      userChoice: $userChoiceInput.value,
+      userName: $userNameInput.value
+    }
+    return choiceInfo
+  }
 }
 
 function renderChoices(data) {
@@ -29,17 +31,84 @@ function renderChoices(data) {
   return $choices
 }
 
-function addChoicesToList($choices) {
-  var $choicesList = document.querySelector('#choices-list')
+function fadeOutAllChoices($choicesList) {
+  var $choices = $choicesList.children
   for (var i = 0; i < $choices.length; i++) {
-    $choicesList.appendChild($choices[i])
+    $choices[i].setAttribute('class', 'choice card-panel animated fadeOut')
   }
 }
 
-function clearAllChoices() {
-  var $choicesList = document.querySelector('#choices-list')
+function clearAllChoices($choicesList) {
   while ($choicesList.firstChild) {
     $choicesList.removeChild($choicesList.firstChild)
+  }
+}
+
+function createRemoveButton() {
+  var $removeButton = document.createElement('button')
+  $removeButton.setAttribute('class', 'btn-floating btn-large waves-effect waves-light red')
+  $removeButton.setAttribute('id', 'remove')
+  $removeButton.textContent = 'X'
+
+  return $removeButton
+}
+
+function removeChoice($choicesList, $choice) {
+  $choice.setAttribute('class', 'choice card-panel animated fadeOut')
+  setTimeout(function () {
+    $choicesList.removeChild($choice)
+  }, 1000)
+}
+
+function randomNumber(data) {
+  if (data.length !== 0) {
+    var number = Math.round(Math.random() * (data.length - 1))
+  }
+  return number
+}
+
+function renderDecision(data, number) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].id === number) {
+      var decision = {
+        winningId: data[i].id,
+        winningChoice: data[i].userChoice,
+        winner: data[i].userName,
+        choices: data
+      }
+      return decision
+    }
+  }
+}
+
+function fadeOutOtherChoices(data) {
+  var $choices = document.querySelectorAll('.choice')
+  for (var i = 0; i < $choices.length; i++) {
+    var choiceDataId = $choices[i].getAttribute('data-id')
+    if (choiceDataId !== data.winningId.toString()) {
+      $choices[i].setAttribute('class', 'choice card-panel animated fadeOut')
+    }
+    else {
+      $choices[i].setAttribute('class', 'choice card-panel')
+    }
+  }
+}
+
+function disableButton($button) {
+  if ($button.getAttribute('id') === 'remove') {
+    $button.setAttribute('class', 'btn-floating btn-large waves-effect waves-light red disabled')
+  }
+  else {
+    $button.setAttribute('class', 'waves-effect waves-light btn-large disabled')
+  }
+}
+
+function enableButton($button) {
+  if ($button.getAttribute('id') === 'remove') {
+    $button.setAttribute('class', 'btn-floating btn-large waves-effect waves-light red')
+  }
+  else {
+    $button.setAttribute('class', 'waves-effect waves-light btn-large')
   }
 }
 
@@ -53,23 +122,55 @@ $submitButton.addEventListener('click', function (e) {
   e.preventDefault()
   var $userChoiceInput = document.querySelector('#user-choice')
   var $userNameInput = document.querySelector('#user-name')
-  var newChoice = choice($userChoiceInput, $userNameInput)
-  choices.push(newChoice)
-  var $choices = renderChoices(choices)
-  addChoicesToList($choices)
-  // clearUserInput($userChoiceInput, $userNameInput)
+  var choiceInfo = createChoiceInfo($userChoiceInput, $userNameInput)
+  if (choiceInfo) {
+    choices.push(choiceInfo)
+    var $choice = choice(choiceInfo)
+    var $removeButton = createRemoveButton()
+    $choice.appendChild($removeButton)
+    var $choicesList = document.querySelector('#choices-list')
+    $choicesList.appendChild($choice)
+    choiceId++
+  }
+  clearUserInput($userChoiceInput, $userNameInput)
 })
 
 var $clearButton = document.querySelector('#clear')
 $clearButton.addEventListener('click', function (e) {
   e.preventDefault()
+  enableButton($submitButton)
+  enableButton($randomizeButton)
   choices = []
-  clearAllChoices()
+  choiceId = 0
+  var $choicesList = document.querySelector('#choices-list')
+  fadeOutAllChoices($choicesList)
+  setTimeout(function () {
+    clearAllChoices($choicesList)
+  }, 1000)
 })
 
 var $randomizeButton = document.querySelector('#randomize')
 $randomizeButton.addEventListener('click', function (e) {
   e.preventDefault()
+  if (choices.length > 1) {
+    var randomNum = randomNumber(choices)
+    var decision = renderDecision(choices, randomNum)
+    fadeOutOtherChoices(decision)
+    var $removeButton = document.querySelectorAll('#remove')[decision.winningId]
+    disableButton($removeButton)
+    disableButton($submitButton)
+    disableButton($randomizeButton)
+  }
+})
+
+var $choicesList = document.querySelector('#choices-list')
+$choicesList.addEventListener('click', function (e) {
+  if (e.target.getAttribute('id') === 'remove') {
+    var $choice = e.target.closest('.choice')
+    var choiceIndex = $choice.getAttribute('data-id')
+    choices.splice(choiceIndex, 1)
+    removeChoice($choicesList, $choice)
+  }
 })
 
 var Typed = require('typed.js')
